7a1e9dc877bd8fd4d89aa10064e3f6eff3edbfa4
arcanjotiago <tiagoarcanjom@gmail.com>
2024-08-13 13:22:57 -0300

    update report services

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Headers} from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post, Query} from '@nestjs/common';
 import { Param } from '@nestjs/common';
 import { ReportService } from './report.service';
 
@@ -12,6 +12,11 @@ export class ReportController {
   @Get(':userid')
   getReportByUser(@Headers('tokenAuthorization') tokenAuthorization:any, @Param('userid') userId:any) {
     return this.reportService.getReportByUser(tokenAuthorization, userId);
+  }
+
+  @Post('costs')
+  postReportCosts(@Headers('tokenAuthorization') tokenAuthorization:any, @Query() date:any) {
+    return this.reportService.postReportCosts(tokenAuthorization, date);
   }
 
 }
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -15,8 +15,48 @@ export class ReportService {
     const tokenValidate:any = await this.authService.checkAccessToken(access_token);
     
     if (tokenValidate.status == 200){
-      return this.orderRepository.findOneBy({ user_id });
+      if(user_id == ':userid'){
+        return {
+          "message":"Please, inform the user id!",
+          "status":400
+        }
+      }
+
+      const response:any = await this.orderRepository.findBy({ user_id });
+
+      if (response == ''){
+        return {
+          "message":"There was no data for user informed!",
+          "status":404
+        }
+      }
+
+      return response;
     }
+
+    return tokenValidate;
+  }
+
+  async postReportCosts(access_token:any, date:any): Promise<any> {
+    const tokenValidate:any = await this.authService.checkAccessToken(access_token);
+    
+    if (tokenValidate.status == 200){
+      
+      const response:any = await this.orderRepository.query(`SELECT * FROM public.order WHERE created_at::timestamp::date = '${date.date}' `)
+
+      let calc = 0;
+      // Aqui vou ter que varrer o array retornado e somar o campo amount
+      for(let i=0; i < response.length; i++){
+        // const x = +response[i].amount;
+        // calc = ( calc + x );
+        console.log(response[i].amount);
+      }
+
+
+      // return calc;
+      return response;
+    }
+    
     return tokenValidate;
   }
 
